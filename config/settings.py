d8b2ae3267fbdc69479f76c9e104086b90d3e7f7
Nasser <tarpaganasser86@gmail.com>
2026-02-01 03:48:48 +0300

    Ajout de STATIC_ROOT pour le déploiement

--- config/settings.py
+++ config/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+# C'est la destination où collectstatic va copier les fichiers
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
